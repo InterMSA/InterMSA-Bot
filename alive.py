235dc6ad28f66d401c283319cd47df3c3edb3793
Baraa Nassar <baraa2aziz@gmail.com>
2021-07-19 02:54:49 -0400

    Manually send,edit,reply  messages
    cool stuff to make the bot interact from a human behind the screen
    added emoji,editin,replying abilities

--- alive.py
+++ alive.py
@@ -6,7 +6,6 @@ Application Name: MSA-Bot
 Functionality Purpose: An agile Discord Bot to fit any MSA's needs
 '''
 
-
 import re, os, sys, time
 from cmds import *
 from config import *
@@ -27,20 +26,67 @@ class Unbuffered(object):
 sys.stdout = Unbuffered(sys.stdout)
 
 # Executes when bot begins running
+
 @bot.event
 async def on_ready():
    await bot.change_presence(activity = Game(name = "/cmds (For all cmds)"))
    print("We have logged in as {0.user} in {1}".format(bot, ENV))
-   try:
-      chan = int(input("Enter Channel ID: "))
-      channel = bot.get_channel(chan)
-   except ValueError:
-      print("Wrong type!")
-      chan = 778053859536273459
-      channel = bot.get_channel(chan)
-   while True:
-      reply = str(input("reply here: "))
-      await channel.send(reply)
+   whatDoUWant = (input("type if you want to\n -'channel message' \n -'edit' \n -'reply'\n -'emoji'\n ==> "))
+
+   if (whatDoUWant.lower() == "channel message"):
+       try:
+          chan = int(input("Enter Channel ID: "))
+          channel = bot.get_channel(chan)
+       except ValueError:
+          print("Wrong type!")
+          chan = 778053859536273459
+          channel = bot.get_channel(chan)
+          
+       while True:
+          reply = str(input("reply here: "))
+          await channel.send(reply)
+#----------------------------------------------------
+   elif (whatDoUWant.lower() == "edit"):
+       print ("ok\n")
+       
+       try:
+           channel = bot.get_channel(int(input("Enter Channel ID: ")))
+           message = await channel.fetch_message((int(input("Enter message ID: "))))
+           con=str(input("the new content of the message is: "))
+           await message.edit(content=f"{con}")
+        
+       except ValueError:
+          print("Wrong type!")
+          '''chan = 778053859536273459
+          channel = bot.get_channel(chan)'''
+ 
+   elif (whatDoUWant.lower() == "reply"):
+       try:
+           channel = bot.get_channel(int(input("Enter Channel ID: ")))
+           message = await channel.fetch_message(int(input("Enter message ID: ")))
+           #rep=
+           await message.reply(str(input("reply here: ")))
+        
+       except ValueError:
+          print("Wrong type!")
+          '''chan = 778053859536273459
+          channel = bot.get_channel(chan)'''
+          
+   elif (whatDoUWant.lower()=="emoji"):
+         channel = bot.get_channel(int(input("Enter Channel ID: ")))
+         message = await channel.fetch_message(int(input("Enter message ID: ")))
+           #rep=
+       #await message.reply(str(input("reply here: ")))
+         thumbsDown = str(input("emoji: ")) #"\U0001F44E" #thumbs down emoji
+         guild = bot.get_guild(SERVER_ID)
+         member= guild.get_member(int(input("user ID: ")))
+         removeOrAdd = str(input("do you want to 'remove' or 'add' the emoji: "))
+         await message.add_reaction(thumbsDown) if removeOrAdd == 'add' else await message.remove_reaction(thumbsDown,member)          
+         #await message.add_reaction(str(input("emoji: ")))
+
+'''ideas'''    
+#maybe we can in the future make it reply with an emoji too
+#make the bot connect to a voice chat
 
 # Standard MSA Bot Commands
 @bot.event
